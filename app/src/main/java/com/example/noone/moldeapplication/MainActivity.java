9c231bd3b94e0a252289a7d557670da506909302
Sawai <sawai@administrators-MacBook-Pro-7.local>
2018-02-14 09:31:56 +0530

    first commit.

---
+++ app/src/main/java/com/example/noone/moldeapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.noone.moldeapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Hide toolbar
+        getSupportActionBar().hide();
+
+        //Load Fragment
+
+
+    }
+}
---
+++ app/src/main/java/com/example/noone/moldeapplication/category/CategoryFragment.java
@@ -0,0 +1,7 @@
+package com.example.noone.moldeapplication.category;
+
+import android.app.Fragment;
+
+public class CategoryFragment extends Fragment {
+
+}
