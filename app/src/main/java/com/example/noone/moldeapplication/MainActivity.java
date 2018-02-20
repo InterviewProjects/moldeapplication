5787aeb42d9a54b2c1927bdd36d99bf2aff396f9
Sawai <sawai@administrators-MacBook-Pro-7.local>
2018-02-20 14:50:02 +0530

    change added.

--- app/src/main/java/com/example/noone/moldeapplication/MainActivity.java
+++ app/src/main/java/com/example/noone/moldeapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.noone.moldeapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.noone.moldeapplication.category.CategoryFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Hide toolbar
-        getSupportActionBar().hide();
+        // set toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //Load Fragment
-
-
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_container, new CategoryFragment())
+                .commit();
     }
 }
